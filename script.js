14b758141fc966f1b1a6f6ad1d7e1a8027396225
Andrés Baez <andresbaez_23@outlook.com>
2025-12-03 14:26:43 -0500

    Inicio página

---
+++ script.js
@@ -0,0 +1,58 @@
+let productos = [];
+
+const btn = document.getElementById("agregar");
+
+const lista = document.getElementById("carrito");
+
+btn.addEventListener("click", () => {
+    const producto = {
+        nombre: btn.dataset.nombre,
+        precio: parseFloat(btn.dataset.precio)
+    }
+    productos.push(producto);
+    mostrarCarrito();
+});
+
+
+
+function actualizarCarrito() {
+    if (productos.length === 0) {
+        alert("El carrito está vacío.");
+    }else {
+        return;
+}
+
+}
+
+function verCarrito(){
+    let verCarrito = document.getElementById("carrito");
+    alert(`Productos en el carrito: ${productos.length}`);
+}
+
+function mostrarCarrito() {
+    lista.innerHTML = "";
+    productos.forEach((producto, index) => {
+        const li = document.createElement("li");
+        li.textContent = `${producto.nombre}` + `- $${producto.precio}`;
+        lista.appendChild(li);
+    });
+}
+
+function indiceCarrito() {
+    document.getElementById("nav_text").textContent = JSON.stringify(productos.length);
+}
+
+function add() {
+    let product = document.getElementById("agregar").verCarrito;
+    productos.push(product);
+    indiceCarrito();
+}
+
+function eliminar(){
+    let eliminarProducto = document.getElementById("eliminar");;
+    productos.pop(eliminarProducto);
+    indiceCarrito();
+    actualizarCarrito();
+}
+
+
